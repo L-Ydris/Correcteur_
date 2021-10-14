40f339f38cdf066c65419f4781d0224e4612cb41
Faris <faris.desvignes@etu.univ-amu.fr>
2021-10-14 15:15:14 +0200

    Levenshtein V2

--- src/java/Levenshtein.java
+++ src/java/Levenshtein.java
@@ -1,19 +1,22 @@
 import java.util.ArrayList;
 
 public class Levenshtein {
-    private String str1;
-    private String str2;
-
-    public Levenshtein(String str1, String str2){
-        this.str1 = str1;
-        this.str2 = str2;
+    private String word1;
+    private String word2;
+    private ArrayList<Character> Char = new ArrayList<Character>();
+    
+    public Levenshtein(String word1, String word2){
+        this.word1 = word1;
+        this.word2 = word2;
     }
 
     public void algo() {
-        for (int i = str1.length()-1; i > 0; i--) {
-            System.out.println(str1.charAt(i));
-            for (int j = str2.length()-1; j > 0; j--) {
-                str2.charAt(j);
+        for (int i = word1.length()-1; i > 0; i--) {
+            System.out.println();
+            for (int j = word2.length()-1; j > 0; j--) {
+                if (word1.charAt(i) == word2.charAt(j)){
+                    word1.charAt(i);
+                }
             }
         }
     }
