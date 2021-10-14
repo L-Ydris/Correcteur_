e7133cc997c19d25b6e9f2a92fcc2b1d78b0d016
Faris <faris.desvignes@etu.univ-amu.fr>
2021-10-14 15:37:22 +0200

    Levenshtein V3

--- src/java/Levenshtein.java
+++ src/java/Levenshtein.java
@@ -3,22 +3,21 @@ import java.util.ArrayList;
 public class Levenshtein {
     private String word1;
     private String word2;
-    private ArrayList<Character> Char = new ArrayList<Character>();
+    private int distance = 0;
     
     public Levenshtein(String word1, String word2){
         this.word1 = word1;
         this.word2 = word2;
     }
 
-    public void algo() {
+    public int distance() {
+        int distance = 0;
         for (int i = word1.length()-1; i > 0; i--) {
             System.out.println();
-            for (int j = word2.length()-1; j > 0; j--) {
-                if (word1.charAt(i) == word2.charAt(j)){
-                    word1.charAt(i);
-                }
-            }
+            for (int j = word2.length()-1; j > 0; j--)
+                if (word1.charAt(i) == word2.charAt(j)) distance = distance+ 1;
         }
+        return distance;
     }
 
     public void insertLetter(char letter){}
--- src/java/Main.java
+++ src/java/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Levenshtein levenshtein = new Levenshtein("heelooo","helloqs");
-        levenshtein.algo();
+        levenshtein.distance();
 
     }
 }
